72f987651820327cdbbf5b2b16d2efd98b238402
mangeshpetkar <49246008+mangeshpetkar@users.noreply.github.com>
2020-07-12 10:02:35 +0530

    Update settings.py

--- Mileblog/settings.py
+++ Mileblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e(&fw44b&jlj*4(^lxdilp6431hkc9w*k0mtbw0us439*37oua'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['milestory-marathi.herokuapp.com','127.0.0.1']
 
